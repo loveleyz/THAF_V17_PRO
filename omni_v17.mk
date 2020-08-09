e227e4ede610e560fae03925a92d0f9ac0506648
loveleyz <loveleyz14@gmail.com>
2020-08-09 20:26:35 +0800

    Update omni_v17.mk

--- omni_v17.mk
+++ omni_v17.mk
@@ -17,7 +17,7 @@
 #
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/pb/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
